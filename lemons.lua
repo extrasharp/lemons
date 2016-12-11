b885012893a140614cac2901162bd0256fca0f67
extrasharp <genericpb@gmail.com>
2016-12-11 17:14:29 -0600

    ya

--- lemons.lua
+++ lemons.lua
@@ -1,5 +1,7 @@
 #!/usr/bin/luajit
 
+local debug_mode = arg[1] == "debug"
+
 local string = require "string"
 local fmt = string.format
 
@@ -77,6 +79,8 @@ local function clone(t)
   return ret
 end
 
+-- base obj
+
 local obj = {
   __call = function (self, ...)
     if self.new then
@@ -116,6 +120,14 @@ function anim:next()
   return self:peek()
 end
 
+function anim:prev()
+  self.cur = self.cur - 1
+  if self.cur < 1 then
+    self.cur = #self
+  end
+  return self:peek()
+end
+
 -- desktops
 
 local desktop_info = obj()
@@ -132,7 +144,7 @@ function desktop_info.new(t)
 end
 
 function desktop_info:set(raw)
-  for i = 0, 5 do
+  for i = 0, self.ct - 1 do
     self.urgent[i + 1] = raw.urgent[i]
     self.windows[i + 1] = raw.windows[i]
   end
@@ -141,13 +153,13 @@ end
 
 function desktop_info:to_str()
   local ret = ""
-  for i = 1, 6 do
+  for i = 1, self.ct do
     local indc = nil
     if i == self.current then
       indc = ":"
-    elseif self.urgent[i] > 0 then
+    elseif self.urgent[i] and self.urgent[i] > 0 then
       indc = "!"
-    elseif self.windows[i] > 0 then
+    elseif self.windows[i] and self.windows[i] > 0 then
       indc = "."
     end
     ret = indc and fmt("%s%s%s ", ret, indc, self.names[i]) or ret
@@ -158,6 +170,9 @@ end
 -- buffer
 
 local buffer = obj()
+buffer.__call = function (self, ...)
+  return self:add(...)
+end
 
 function buffer.new()
   return setmetatable({ "" }, buffer)
@@ -169,26 +184,36 @@ end
 
 function buffer:add(text)
   self[1] = fmt("%s%s", self[1], text or "")
+  return self
 end
 
 function buffer:to_str()
   return self[1]
 end
 
-function buffer:set_pos(pos)
-  local t = { l = "%{l}", r = "%{r}", c = "%{c}" }
-  self:add(t[pos] or "")
+local pos = {
+  l = "%{l}" ,
+  r = "%{r}" ,
+  c = "%{c}" ,
+}
+
+local colors = {
+  bg = "#191919" ,
+  fg = "#AAAAAA" ,
+  reset = "-"    ,
+}
+
+local function colorize(fgc, bgc)
+  return fmt("%s%s"
+    , fgc and fmt("%%{F%s}", fgc) or ""
+    , bgc and fmt("%%{B%s}", bgc) or "")
 end
 
-function buffer:set_colors(fgc, bgc)
-  self:add(fmt("%s%s"
-    , fgc and fmt("%%{F%s}", fgc) or ""
-    , bgc and fmt("%%{B%s}", bgc) or ""))
-end
+-- info get
 
 local sw = false
 
-function buffer:put_batt()
+local function batt()
   local capa = file_to_str("/sys/class/power_supply/BAT0/capacity")
   local stat = file_to_str("/sys/class/power_supply/BAT0/status"):sub(1, 1)
   local capa_int = tonumber(capa) or 0
@@ -201,142 +226,151 @@ function buffer:put_batt()
     end
   end
   sw = not sw
-  self:add(fmt("%d", capa_int))
-  self:add(status_strs[stat] or "?")
+  return fmt("%d%s", capa_int, status_strs[stat] or "?")
 end
 
 local paused = anim {
   "   (__" ,
   "   (__" ,
-  "   (__" ,
+  "  .(__" ,
   "  z(__" ,
-  " z (__" ,
+  " Z (__" ,
   "z  (__" ,
 }
 
-function buffer:put_mpd()
-  local tags = {}
-  function mpd_get(tag)
-    if tag == "paused" then
-      local f = io.popen("mpc -f ''")
-      if not f then goto out end
-      f:read("*l")
-      local status = f:read("*l") or ""
-      tags.playing = status:find("playing")
-      f:close()
-      goto out
+local sep = "║"
+
+local mpc_find = {
+  "artist", "title", "file", "album"
+}
+local mpc_cmd = "mpc -f '"
+for i = 1, #mpc_find do
+  mpc_cmd = fmt("%s%%%s%%#%s", mpc_cmd, mpc_find[i], sep)
+end
+mpc_cmd = fmt("%s'", mpc_cmd)
+
+local last_info
+local tags = {}
+
+local spin = {"-", "/", "\\"}
+local sp1 = anim(spin)
+local sp2 = anim(spin)
+
+local oh = {"o ",  " o"}
+local oh1 = anim(oh)
+local oh2 = anim(oh)
+
+local function mpd()
+  local f = io.popen(mpc_cmd)
+  if not f then return end
+  local info = f:read("*l") or ""
+  local status = f:read("*l") or ""
+  f:close()
+
+  if last_info ~= info then
+    local idx = 1
+    for str in info:gmatch(fmt("([^%s]*)%s", sep, sep)) do
+      if str == "" then str = nil end
+      tags[mpc_find[idx]] = str
+      idx = idx + 1
     end
 
-    do
-      local get = cmd_to_str(fmt("mpc -f '%%%s%%'", tag), "*l")
-      if get ~= "" then
-        tags[tag] = get
-      end
-    end
-
-    ::out::
-    return mpd_get
+    _, _, tags.file = tags.file:find("([^/]+%.%w+)")
   end
 
-  mpd_get "artist" "title" "file" "album" "paused"
-  local _, _, file = tags.file:find("(%w+%.%w+)")
-  self:add(tags.playing
-    and fmt("%s >> %s >> %s", tags.artist, tags.title or file, tags.album)
-    or  paused:next())
+  last_info = info
+
+  local playing = status:find("playing")
+  return playing and
+    fmt("%s %s %s %s %s"
+      , tags.artist or ""
+      , tags.artist and oh1:next() or sp1:next()
+      , tags.title  or tags.file
+      , tags.album  and oh2:next() or sp2:prev()
+      , tags.album  or "")
+    or paused:next()
 end
 
-function buffer:put_time()
+local function time()
   local time = cmd_to_str("date +'%I%M%S'")
   local hr  = time:sub(1, 2)
   local min = time:sub(3, 4)
   local sec = tonumber(time:sub(5, 6)) or 0
   sec = sec - (sec % 3)
-  local time = fmt("♡ %s:%s.%02d ♡", hr, min, sec)
-  self:add(time)
+  return fmt("%s:%s.%02d", hr, min, sec)
 end
 
--- main
+-- setup
 
-function main()
-  local mqd = -1
-  while mqd < 0 do
-    mqd = rt.mq_open("/monsterwm", O.RDONLY)
-    ffi.C.poll(nil, 0, 100)
-  end
+local mqd = -1
+while mqd < 0 do
+  mqd = rt.mq_open("/monsterwm", O.RDONLY)
+  ffi.C.poll(nil, 0, 100)
+end
 
-  local pfds = ffi.new("struct pollfds[1]")
-  pfds[0].fd = mqd
-  pfds[0].events = POLL.IN
+local pfds = ffi.new("struct pollfds[1]")
+pfds[0].fd = mqd
+pfds[0].events = POLL.IN
 
-  local raw_info = ffi.new("struct msg[1]");
+local raw_info = ffi.new("struct msg[1]");
 
-  local info = desktop_info {
-    "lov♡" , "kis♡" , "ya ♡" ,
-    "2 ♡♡" , "♡jx " , "(vv)" ,
-  }
+local bar = debug_mode and io.stdout
+or
+  io.popen(fmt(
+  '~/dotfiles/_wm/bar/lemonbar -f "tewi:pixelsize=10"'..
+  ' -f "Kochi Gothic:pixelsize=10:antialias=false"'..
+  ' -B "%s" -F "%s" -g x12', colors.bg, colors.fg), "w")
 
-  local colors = {
-    bg = "#191919" ,
-    fg = "#AAAAAA" ,
-    reset = "-"    ,
-  }
+local buf = buffer()
 
-  local star_wink = anim {
-    " (oo" ,
-    " (oo" ,
-    "★(-o" ,
-  }
+local info = desktop_info {
+  "lov♡" , "kis♡" , "ya ♡" ,
+  "2 ♡♡" , "♡jx " , "(vv)" ,
+}
 
-  local bar = arg[1] == "debug" and io.stdout
-  or
-    io.popen(fmt(
-    '~/dotfiles/_wm/bar/lemonbar -f "tewi:pixelsize=10"'..
-    ' -f "Kochi Gothic:pixelsize=10:antialias=false"'..
-    ' -B "%s" -F "%s" -g x12', colors.bg, colors.fg), "w")
+local wink = anim {
+  " (oo" ,
+  " (oo" ,
+  "★(-o" ,
+}
 
-  local buf = buffer()
+-- loop
 
-  while true do
-    local res = ffi.C.poll(pfds[0], 1, 1000);
-    if res > 0 then
-      if (bit.band(pfds[0].revents, POLL.IN) ~= 0) then
-        rt.mq_receive(mqd, ffi.cast("char *", raw_info), ffi.sizeof("struct msg"), nil);
-        if raw_info[0].do_quit == 1 then
-          io.write "goodbye\n"
-          break
-        else
-          info:set(raw_info[0])
-        end
+while true do
+  local res = ffi.C.poll(pfds[0], 1, 1000);
+  if res > 0 then
+    if (bit.band(pfds[0].revents, POLL.IN) ~= 0) then
+      rt.mq_receive(mqd, ffi.cast("char *", raw_info), ffi.sizeof("struct msg"), nil);
+      if raw_info[0].do_quit == 1 then
+        break
+      else
+        info:set(raw_info[0])
       end
     end
-
-    buf:clear()
-
-    buf:set_pos("l")
-    buf:set_colors(colors.bg, colors.fg)
-    buf:add(info:to_str())
-    buf:set_colors(colors.fg, colors.bg)
-    buf:add("")
-
-    buf:set_pos("c")
-    buf:put_mpd()
-
-    buf:set_pos("r")
-    buf:add("")
-    buf:set_colors(colors.bg, colors.fg)
-    buf:add(star_wink:next())
-    buf:add(" ")
-    buf:put_time()
-    buf:add(" ")
-    buf:put_batt()
-
-    buf:set_colors(colors.reset, colors.reset)
-
-    bar:write(buf:to_str(), "\n")
-    bar:flush()
   end
 
-  bar:close()
+  buf:clear()
+
+  buf:add(pos.l)
+  buf:add(colorize(colors.bg, colors.fg))
+  buf:add(info:to_str())
+  buf:add(colorize(colors.fg, colors.bg))
+  buf:add("")
+
+  buf:add(pos.c)
+  buf:add(mpd())
+
+  buf:add(pos.r)
+  buf:add("")
+  buf:add(colorize(colors.bg, colors.fg))
+  buf:add(wink:next()) " ♡ " (time()) " ♡ " (batt())
+  buf:add(colorize(colors.reset, colors.reset))
+
+  bar:write(buf:to_str(), "\n")
+  bar:flush()
 end
 
-main()
+-- see you
+
+bar:close()
+io.write("goodbye\n")
