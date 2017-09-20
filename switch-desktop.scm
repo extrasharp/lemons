e039f8f4ec8c6a76ec2f17c2d1f91d695d89c644
mel aise <genericpb@gmail.com>
2017-09-19 23:42:44 -0500

    use 'directory'

--- switch-desktop.scm
+++ switch-desktop.scm
@@ -2,27 +2,32 @@
 
 (define args (cdddr (argv)))
 (when (or (null? args)
-        (not (member (car args) '("next" "prev"))))
+          (not (member (car args) '("next" "prev")))
+          )
   (exit))
 (define cmd (car args))
 
-(use utils)
+(use
+  (srfi 1)
+  posix
+  utils
+  )
 
-(define files '#(
-  "/home/mel/images/bgs/acid_fast_leprae.jpg"
-  "/home/mel/images/bgs/alien-nine.jpg"
-  "/home/mel/images/bgs/bacteria.jpg"
-  "/home/mel/images/bgs/chip.jpg"
-  "/home/mel/images/bgs/yellow.png"
-  ; "/home/mel/images/bgs/bw.jpg"
-  ; "/home/mel/images/bgs/yaya.jpg"
-  ; "/home/mel/images/bgs/ring.png"
-  ; "/home/mel/images/bgs/chalkboard.jpg"
-  ; "/home/mel/images/bgs/HQPT32.jpg"
-  ; "/home/mel/images/bgs/tAG_21252.jpg"
-  ))
+(define bg-dir "/home/mel/images/bgs")
 
-(define last-feh (with-input-from-file "/home/mel/.fehbg" read-all))
+(define files
+  (list->vector
+    (filter-map
+      (lambda (f)
+        (let (
+          (f (string-append bg-dir "/" f))
+          )
+          (if (directory? f) #f f)
+          ))
+      (directory bg-dir))
+    ))
+
+(define last-feh (read-all "/home/mel/.fehbg"))
 (define last-desktop
   (irregex-match-substring
     (irregex-search
@@ -30,24 +35,22 @@
     1)
   )
 
-(define next #f)
-
-(let loop (
-  (n 0)
-  )
-  (cond
-    ((>= n (vector-length files))
-      (set! next 0))
-    ((string=? last-desktop (vector-ref files n))
-      (set! next
+(define next
+  (let loop (
+    (n 0)
+    )
+    (cond
+      ((>= n (vector-length files))
+        0)
+      ((string=? last-desktop (vector-ref files n))
         (modulo
-          (if (string=? cmd "next")
-              (+ n 1)
-              (- n 1))
-          (vector-length files))))
-    (else
-      (loop (+ n 1)))
-    ))
+          ((if (string=? cmd "next") + -) n 1)
+          (vector-length files)))
+      (else
+        (loop (+ n 1)))
+      ))
+  )
 
-(system (string-append
-  "feh --bg-fill " (vector-ref files next)))
+(system
+  (string-append
+    "feh --bg-fill " (vector-ref files next)))
